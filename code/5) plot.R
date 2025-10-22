f631cd935820ff8b5b7605b3d2bfc9bec669a113
Jeff Wesner <jeff.wesner@usd.edu>
2025-10-22 18:27:30 -0500

    fix plots

--- code/5) plot.R
+++ code/5) plot.R
@@ -5,6 +5,7 @@ library(tidybayes)
 library(brms)
 library(ggh4x)
 library(ggtext)
+library(patchwork)
 
 
 source("code/custom_functions.R")
@@ -35,10 +36,7 @@ brm_total_snails = readRDS("models/brm_total_snails.rds")
 # get model data
 mod_d = brm_taxon_mac_fine_vel$data
 
-# get raw data
-snail_density = readRDS(file = "data/snail_density.rds")
-
-# make predictors
+# make predictor
 taxon_predictor_grid = get_predictor_grid(brm_total_snails, velocity_s, macrophyte_s, fines_s) %>% 
   expand_grid(taxon = unique(mod_d$taxon))
 
@@ -50,35 +48,37 @@ taxon_posts = taxon_predictor_grid %>%
   add_epred_draws(brm_taxon_mac_fine_vel, re_formula = ~ (1 + macrophyte_s + fines_s + velocity_s|taxon)) %>% 
   pivot_longer(cols = ends_with("_s")) %>% 
   filter(value != 0) %>% 
-  mutate(taxon_order = case_when(taxon == "Pyrgulopsis" ~ "b) *Pyrgulopsis*",
-                                 taxon == "Fossaria" ~ "c) *Fossaria*",
-                                 TRUE ~ "d) *Physa*")) %>% 
+  mutate(taxon_order = case_when(taxon == "Pyrgulopsis" ~ "B *Pyrgulopsis*",
+                                 taxon == "Galba" ~ "C *Galba*",
+                                 taxon == "Fossaria" ~ "C *Galba*",
+                                 TRUE ~ "D *Physa*")) %>% 
   mutate(taxon = case_when(taxon == "Pyrgulopsis" ~ "*Pyrgulopsis*",
-                           taxon == "Fossaria" ~ "*Fossaria*",
+                           taxon == "Galba" ~ "*Galba*",
+                           taxon == "Fossaria" ~ "*Galba*",
                            taxon == "Physa" ~ "*Physa*",
-                           TRUE ~ "All Snails")) 
+                           TRUE ~ "All snails")) 
 
 total_posts = total_predictor_grid %>%
   add_epred_draws(brm_total_snails, re_formula = NA) %>% 
   pivot_longer(cols = ends_with("_s")) %>% 
   filter(value != 0) %>% 
-  mutate(taxon = "All Snails",
-         taxon_order = "a) All Snails") 
+  mutate(taxon = "All snails",
+         taxon_order = "A All snails") 
 
 
 # bind posteriors 
 post_preds_regression = bind_rows(total_posts, taxon_posts) %>% 
          mutate(predictor = name,
-                formatted_predictor = case_when(name == "fines_s" ~ "% Fine Sediment",
+                formatted_predictor = case_when(name == "fines_s" ~ "% Fine sediment",
                                      name == "macrophyte_s" ~ "% Macrophytes",
-                                     TRUE ~ "Stream Velocity"),
-                numbered_predictor = case_when(name == "fines_s" ~ "a) % Fine Sediment",
-                                   name == "macrophyte_s" ~ "b) % Macrophytes",
-                                   TRUE ~ "c) Stream Velocity")) %>% 
+                                     TRUE ~ "Stream velocity"),
+                numbered_predictor = case_when(name == "fines_s" ~ "A % Fine sediment",
+                                   name == "macrophyte_s" ~ "B % Macrophytes",
+                                   TRUE ~ "C Stream velocity")) %>% 
   left_join(predictors_mean_sd) %>% 
   mutate(pred_values = (value*sd) + mean) %>% 
   mutate(taxon = as.factor(taxon),
-         taxon = fct_relevel(taxon, "All Snails"))
+         taxon = fct_relevel(taxon, "All snails"))
 
 saveRDS(post_preds_regression, file = "posteriors/post_preds_regression.rds")
 
@@ -89,65 +89,92 @@ pred_data_taxon = mod_d %>%
                values_to = "pred_values",
                names_to = "predictor")  %>% 
   mutate(name = predictor,
-         formatted_predictor = case_when(name == "fines_s" ~ "% Fine Sediment",
+         formatted_predictor = case_when(name == "fines_s" ~ "% Fine sediment",
                                          name == "macrophyte_s" ~ "% Macrophytes",
-                                         TRUE ~ "Stream Velocity"),
-         numbered_predictor = case_when(name == "fines_s" ~ "a) % Fine Sediment",
-                                        name == "macrophyte_s" ~ "b) % Macrophytes",
-                                        TRUE ~ "c) Stream Velocity")) %>% 
+                                         TRUE ~ "Stream velocity"),
+         numbered_predictor = case_when(name == "fines_s" ~ "A % Fine sediment",
+                                        name == "macrophyte_s" ~ "B % Macrophytes",
+                                        TRUE ~ "C Stream velocity")) %>% 
   left_join(predictors_mean_sd) %>% 
   mutate(pred_values = (pred_values*sd) + mean, 
          .epred = taxon_snail) %>% 
   mutate(taxon = case_when(taxon == "Pyrgulopsis" ~ "*Pyrgulopsis*",
-                           taxon == "Fossaria" ~ "*Fossaria*",
+                           taxon == "Galba" ~ "*Galba*",
+                           taxon == "Fossaria" ~ "*Galba*",
                            taxon == "Physa" ~ "*Physa*",
-                           TRUE ~ "All Snails")) 
+                           TRUE ~ "All snails")) 
 
 
 pred_data_total = pred_data_taxon %>% group_by(.epred, pred_values, site_f, quadrat_f, transect_f, 
                                                name, predictor, formatted_predictor, numbered_predictor) %>% 
   reframe(.epred = sum(.epred)) %>% 
-  mutate(taxon = "All Snails")
+  mutate(taxon = "All snails")
 
 pred_data_all = bind_rows(pred_data_taxon, pred_data_total) %>% 
   mutate(taxon = as.factor(taxon),
-         taxon = fct_relevel(taxon, "All Snails"))
+         taxon = fct_relevel(taxon, "All snails"))
 
 # plot_posteriors 
 
-plot_density_regression = post_preds_regression %>% 
-  filter(.draw <= 1000) %>% 
-  ggplot(aes(x = pred_values, y = .epred + 1)) + 
-  stat_lineribbon(.width = c(0.5, 0.75, 0.95), aes(fill = predictor),
-                  alpha = 0.3, linewidth = 0.1) +
-  facet_grid2(taxon~numbered_predictor, scales = "free_x") +
-  geom_point(data = pred_data_all,
-             size = 0.1, shape = ".") +
-  scale_y_log10() +
-  labs(x = "Predictor Value",
-       y = "Total Snails per Quadrat + 1",
-       fill = "",
-       color = "") +
-  guides(fill = "none",
-         color = "none") +
-  ggthemes::scale_fill_colorblind() +
-  theme_default() +
-  theme(strip.text.y = element_markdown(),
-        strip.text.x = element_text(hjust = 0)) +
-  NULL
+make_fig2 = function(posteriors = NA, 
+         raw_data = NA){
+  
+  plot_data = posteriors
+  
+  plot_data %>% 
+    filter(.draw <= 1000) %>% 
+    ggplot(aes(x = pred_values, y = .epred*4 + 1)) + 
+    stat_lineribbon(.width = c(0.5, 0.75, 0.95), aes(fill = predictor),
+                    alpha = 0.3, linewidth = 0.1) +
+    facet_grid2(taxon~numbered_predictor, scales = "free_x") +
+    geom_point(data = pred_data_all %>% 
+                 filter(taxon == unique(plot_data$taxon)),
+               size = 0.1, shape = ".") +
+    scale_y_log10() +
+    labs(x = "Predictor value",
+         y = bquote("Total snails/m"^2 ~ "+ 1"),
+         fill = "",
+         color = "") +
+    guides(fill = "none",
+           color = "none") +
+    ggthemes::scale_fill_colorblind() +
+    theme_classic() +
+    theme(strip.background = element_blank(),
+          strip.text.y = element_markdown(),
+          strip.text.x = element_text(hjust = 0)) +
+    NULL
+}
 
-ggsave(plot_density_regression, file = "plots/plot_density_regression.tif", width = 6.5, height = 8, bg = "white")
-ggsave(plot_density_regression, file = "plots/plot_density_regression.jpg", width = 6.5, height = 8, bg = "white")
+post_preds_regression_list = post_preds_regression %>% group_by(taxon) %>% group_split()
+
+figure_2_list = list()
+
+for(i in 1:length(post_preds_regression_list)){
+  figure_2_list[[i]] = make_fig2(posteriors = post_preds_regression_list[[i]])
+}
+
+plot_density_regression = (figure_2_list[[1]] + theme(axis.text.x = element_blank()))/
+  (figure_2_list[[2]] + theme(axis.text.x = element_blank()))/
+  (figure_2_list[[3]] + theme(axis.text.x= element_blank()))/
+  (figure_2_list[[4]] + theme(axis.text.x = element_blank()))/
+  plot_layout(axis_titles = "collect")
+
+
+
+ggsave(plot_density_regression, file = "plots/fig_2_plot_density_regression.tif", width = 6.5, height = 8, bg = "white")
+ggsave(plot_density_regression, file = "plots/fig_2_plot_density_regression.jpg", width = 6.5, height = 8, bg = "white")
 
 # capture probabilities -----------------------------------------------
 capture_prob_taxon = mod_d %>% 
   select(taxon, site_f, taxon_snail) %>% 
   mutate(zi = 1 - case_when(taxon_snail == 0 ~ 0, TRUE ~ 1)) %>% 
-  mutate(taxon_order = case_when(taxon == "Pyrgulopsis" ~ "c) *Pyrgulopsis*",
-                                 taxon == "Fossaria" ~ "b) *Fossaria*",
-                                 TRUE ~ "d) *Physa*"))
+  mutate(taxon_order = case_when(taxon == "Pyrgulopsis" ~ "B *Pyrgulopsis*",
+                                 taxon == "Galba" ~ "C *Galba*",
+                                 taxon == "Fossaria" ~ "C *Galba*",
+                                 TRUE ~ "D *Physa*"))
 
-capture_prob_data = bind_rows(capture_prob_taxon, capture_prob_taxon %>% mutate(taxon_order = "a) All Snails"))
+capture_prob_data = bind_rows(capture_prob_taxon, capture_prob_taxon %>% mutate(taxon_order = "A All snails",
+                                                                                taxon = "All snails")) 
 
 capture_probs_taxa = mod_d %>% 
   select(taxon, site_f) %>% 
@@ -159,34 +186,69 @@ capture_probs_taxa = mod_d %>%
 capture_probs_overall = tibble(site_f = unique(snail_density$site_f)) %>%
   mutate(macrophyte_s = 0, fines_s = 0, velocity_s = 0) %>% 
   add_epred_draws(brm_total_snails, re_formula = ~(1|site_f), dpar = "zi") %>% 
-  mutate(taxon = "All Snails",
-         taxon_order = "a) All Snails")
+  mutate(taxon = "All snails",
+         taxon_order = "A All snails")
 
 capture_probs = bind_rows(capture_probs_taxa, capture_probs_overall)
 
 saveRDS(capture_probs, file = "posteriors/capture_probs.rds")
 
-plot_capture_probs = capture_probs %>% 
-  ggplot(aes(x = site_f, y = 1 - zi, fill = taxon_order)) +
-  geom_line(data = . %>% filter(.draw <= 500),
-            aes(group = .draw, color = taxon_order), alpha = 0.02) + 
-  # geom_violin(linewidth = 0.2) + 
-  geom_boxplot(aes(group = site_f), outlier.shape = NA, width = 0.5) +
-  geom_jitter(data = capture_prob_data, width = 0.2, height = 0,
-              alpha = 0.6, shape = "|") +
-  labs(x = "(upstream)                Site                (downstream)",
-       y = "Occurrence Probability") +
-  guides(color = "none",
-         fill = "none") +
-  facet_wrap(~taxon_order, ncol = 1) +
-  scale_color_brewer(type = 'qual') + 
-  scale_fill_brewer(type = 'qual') +
-  theme_default() +
-  theme(strip.text = element_markdown(hjust = 0)) +
-  NULL
 
-ggsave(plot_capture_probs, file = "plots/plot_capture_probs.jpg", width = 5, height = 9)
-ggsave(plot_capture_probs, file = "plots/plot_capture_probs.tif", width = 5, height = 9, bg = "white")
+
+make_fig3 = function(posteriors = NA, 
+                     raw_data = NA){
+  
+  plot_data = posteriors
+  colors = unique(plot_data$fill)
+  
+  plot_data %>% 
+    ggplot(aes(x = site_f, y = 1 - zi), fill = colors) +
+    geom_line(data = . %>% filter(.draw <= 500),
+              aes(group = .draw),
+              color = colors,
+              alpha = 0.02) + 
+    # geom_violin(linewidth = 0.2) + 
+    geom_boxplot(aes(group = site_f), outlier.shape = NA, width = 0.5) +
+    geom_jitter(data = capture_prob_data %>% 
+                  filter(taxon == unique(plot_data$taxon)), width = 0.2, height = 0,
+                alpha = 0.6, shape = "|") +
+    labs(x = "Upstream    \u27F5  Reach  \u27F6      Downstream",
+         y = "Occurrence probability") +
+    guides(color = "none",
+           fill = "none") +
+    facet_wrap(~taxon_order, ncol = 1) +
+    scale_color_brewer(type = 'qual') + 
+    scale_fill_brewer(type = 'qual') +
+    theme_classic() +
+    theme(strip.background = element_blank(),
+          strip.text = element_markdown(hjust = 0)) +
+    NULL
+  
+}
+
+capture_probs_list = capture_probs %>% 
+  group_by(taxon_order) %>% 
+  mutate(PANEL = case_when(taxon == "Pyrgulopsis" ~ "2",
+                           taxon == "Fossaria" ~ "3",
+                           taxon == "Physa" ~ "4",
+                           TRUE ~ "1")) %>% 
+  left_join(color_list) %>% 
+  group_split()
+
+figure_3_list = list()
+
+for(i in 1:length(capture_probs_list)){
+  figure_3_list[[i]] = make_fig3(posteriors = capture_probs_list[[i]])
+}
+
+plot_capture_probs = (figure_3_list[[1]] + theme(axis.text.x = element_blank()))/
+  (figure_3_list[[2]] + theme(axis.text.x = element_blank()))/
+  (figure_3_list[[3]] + theme(axis.text.x= element_blank()))/
+  (figure_3_list[[4]] + theme(axis.text.x = element_blank()))/
+  plot_layout(axis_titles = "collect")
+
+ggsave(plot_capture_probs, file = "plots/fig_3_plot_capture_probs.jpg", width = 5, height = 9)
+ggsave(plot_capture_probs, file = "plots/fig_3_plot_capture_probs.tif", width = 5, height = 9, bg = "white")
 
 
 # mean density by site ----------------------------------------------------
@@ -197,7 +259,7 @@ site_posts = brm_total_snails$data %>%
   add_epred_draws(brm_total_snails) %>% 
   group_by(.draw, site_f) %>% 
   reframe(.epred = mean(.epred)) %>% 
-  mutate(taxon = "All Snails")
+  mutate(taxon = "All snails")
 
 site_posts_taxon = brm_taxon_mac_fine_vel$data %>% 
   select(-taxon_snail) %>% 
@@ -208,14 +270,15 @@ site_posts_taxon = brm_taxon_mac_fine_vel$data %>%
 
 site_posts_all = bind_rows(site_posts, site_posts_taxon) %>% 
   mutate(taxon = case_when(taxon == "Pyrgulopsis" ~ "*Pyrgulopsis*",
-                           taxon == "Fossaria" ~ "*Fossaria*",
+                           taxon == "Galba" ~ "*Galba*",
+                           taxon == "Fossaria" ~ "*Galba*",
                            taxon == "Physa" ~ "*Physa*",
-                           TRUE ~ "All Snails")) %>% 
+                           TRUE ~ "All snails")) %>% 
   left_join(post_preds_regression  %>% ungroup %>% distinct(taxon, taxon_order))
 
 saveRDS(site_posts_all, file = "posteriors/site_posts_all.rds")
 
-pred_data_all %>% 
+temp = pred_data_all %>% 
   left_join(post_preds_regression  %>% ungroup %>% distinct(taxon, taxon_order)) %>% 
   ggplot(aes(x = site_f, y = .epred + 1)) + 
   geom_point(aes(fill = taxon_order), shape = 21, color = "black",
@@ -223,41 +286,89 @@ pred_data_all %>%
   facet_wrap(~taxon_order) +
   tidybayes::stat_pointinterval(data = site_posts_all) + 
   scale_y_log10() +
-  labs(x = "(upstream)                Site                (downstream)",
+  labs(x = "(upstream)  <---------  Reach --------->       (downstream)",
        y = "Total Snails per Quadrat + 1",
        fill = "",
        color = "") +
   guides(fill = "none",
          color = "none") +
   scale_fill_brewer(type = 'qual') + 
-  theme_default() +
-  theme(strip.text = element_markdown(hjust = 0)) +
+  theme_classic() +
+  theme(strip.text = element_markdown(hjust = 0),
+        strip.background = element_blank()) +
   NULL
 
 
+temp_data = ggplot_build(temp)$data
 
-plot_density_site = site_posts_all %>% 
-  ggplot(aes(x = site_f, y = .epred + 1, fill = taxon_order)) +
+color_list = temp_data[[1]] %>% distinct(fill, PANEL)
+
+site_posts_all = readRDS(file = "posteriors/site_posts_all.rds")
+
+
+
+make_fig_1 = function(posteriors = NA, raw_data = NA){ 
+  plot_data = posteriors
+  colors = unique(plot_data$fill)
+  
+  plot_data %>% 
+  ggplot(aes(x = site_f, y = .epred*4 + 1*4)) +
   geom_line(data = . %>% filter(.draw <= 500),
-            aes(group = .draw, color = taxon_order), alpha = 0.02) + 
-  # geom_violin(linewidth = 0.2) + 
-  geom_boxplot(aes(group = site_f), outlier.shape = NA, width = 0.5) +
-  geom_jitter(data = pred_data_all %>% 
-                left_join(post_preds_regression  %>% ungroup %>% distinct(taxon, taxon_order)),
+            aes(group = .draw),
+            color = colors,
+            alpha = 0.02) + 
+  # geom_violin(linewidth = 0.2) +
+    geom_boxplot(aes(group = site_f), outlier.shape = NA, width = 0.5,
+                 fill = colors) +
+  geom_jitter(data = raw_data %>% 
+                left_join(post_preds_regression %>% ungroup %>% distinct(taxon, taxon_order)) %>%
+                filter(taxon_order == unique(plot_data$taxon_order)),
               size = 0.4,
               width = 0.05, height = 0,
               alpha = 0.6) +
-  labs(x = "(upstream)                Site                (downstream)",
-       y = "Total Snails per Quadrat + 1") +
+  labs(x = "",
+       y = bquote("Total snails/m"^2 ~ "+ 1")) +
   guides(color = "none",
          fill = "none") +
-  facet_wrap(~taxon_order, ncol = 1) +
+  facet_wrap(~taxon_order) +
   scale_color_brewer(type = 'qual') + 
   scale_fill_brewer(type = 'qual') +
-  scale_y_log10() +
-  theme_default() +
-  theme(strip.text = element_markdown(hjust = 0)) +
+  scale_y_log10(breaks = c(1, 10, 100, 1000),
+                labels = c("1", "10", "100", "1000"),
+                limits = c(1, NA)) +
+  theme_classic() +
+  theme(strip.background = element_blank(),
+        strip.text = element_markdown(hjust = 0)) +
   NULL
+}
 
-ggsave(plot_density_site, file = "plots/plot_density_site.jpg", width = 5, height = 9)
-ggsave(plot_density_site, file = "plots/plot_density_site.tif", width = 5, height = 9, bg = "white")
+site_posts_all_list = site_posts_all %>% arrange(taxon_order) %>% 
+  group_by(taxon_order) %>% 
+  mutate(PANEL = case_when(taxon == "*Pyrgulopsis*" ~ "2",
+                           taxon == "*Galba*" ~ "3",
+                           taxon == "*Physa*" ~ "4",
+                           TRUE ~ "1")) %>% 
+  left_join(color_list) %>% 
+  group_split()
+
+fig_1_list = list()
+
+for(i in 1:length(site_posts_all_list)){
+  fig_1_list[[i]] = make_fig_1(raw_data = pred_data_all,
+                               posteriors = site_posts_all_list[[i]])
+}
+
+library(patchwork)
+plot_density_site = (fig_1_list[[1]] + theme(axis.text = element_blank()))/
+ (fig_1_list[[2]] + theme(axis.text = element_blank()))/
+  (fig_1_list[[3]] + theme(axis.text = element_blank()))/
+  fig_1_list[[4]] +
+  plot_layout(axis_titles = "collect")
+
+plot_density_site = plot_density_site + labs(x = "Upstream    \u27F5  Reach  \u27F6      Downstream")
+
+ggsave(plot_density_site, file = "plots/fig_1_plot_density_site.jpg", width = 5, height = 9)
+ggsave(plot_density_site, file = "plots/fig_1_plot_density_site.tif", width = 5, height = 9, bg = "white")
+
+
+
